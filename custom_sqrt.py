4aa5fad4109b406a3295ebffceebc9853964b45e
WillBAnders <WillBAnders@gmail.com>
2022-01-18 17:48:47 -0500

    fix issue

--- custom_sqrt.py
+++ custom_sqrt.py
@@ -1,9 +1,11 @@
-from math import sqrt
+import math
 
 def custom_sqrt(number):
+    if (math.isnan(number)):
+        raise Exception("Invalid number")
     if (number < 0):
-        return -sqrt(-number)
-    return sqrt(number)
+        return -math.sqrt(-number)
+    return math.sqrt(number)
 
 number = input("Enter a number: ")
 print(custom_sqrt(float(number)))
